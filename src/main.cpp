4e1d64aa130e56339051d9916a13d31e48e82293
Arthur SIMON <asimon@e1r13p12.clusters.42paris.fr>
2022-10-15 00:38:05 +0200

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,11 @@
 /*   By: asimon <asimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 13:34:53 by asimon            #+#    #+#             */
-/*   Updated: 2022/10/08 23:56:47 by asimon           ###   ########.fr       */
+/*   Updated: 2022/10/15 00:35:13 by asimon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../templates/functions/is_integral.hpp"
-# include "../templates/functions/enable_if.hpp"
-# include "../templates/iterator/iterator_traits.hpp"
-# include "../templates/iterator/iterator.hpp"
+# include "../private/ftnspace.hpp"
 # include <iostream>
 # include <vector>
 # include <iterator>
@@ -25,14 +22,8 @@ class A{};
 
 int		main(int ac, char **av)
 {
-	// int		*tab = new int [10];
-	std::vector<int>			tab;
+	ft::vector<int>			tab(10);
 
-	// for (int i = 0; i < 10; i++){
-	// 	tab[i] = 100 - i;
-	// }
-
-	std::cout << "max:  " << tab.max_size() << std::endl;
-
+	std::cout << tab.at(0) << std::endl;
 	return (0);
 }
