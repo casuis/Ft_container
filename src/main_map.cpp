ac480c5f6fd90915afaf085892c3cd76028e75dd
Arthur SIMON <asimon@e2r9p17.clusters.42paris.fr>
2022-12-26 18:58:19 +0100

    add fctns add/srch/delete for bst value

--- src/main_map.cpp
+++ src/main_map.cpp
@@ -6,7 +6,7 @@
 /*   By: asimon <asimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 12:14:31 by asimon            #+#    #+#             */
-/*   Updated: 2022/12/26 14:23:47 by asimon           ###   ########.fr       */
+/*   Updated: 2022/12/26 18:57:04 by asimon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,37 @@ int main(void){
 	std::cout << std::endl << YELLOW << "##########MAP##########" << RESET << std::endl << std::endl;
 	{	
 		ft::Bst<int, int>		bst;
+		ft::Node<int, int>		*tmp;
 		ft::Node<int, int>		first_node(1, 100);
 		ft::Node<int, int>		second_node(2, 99);
 		ft::Node<int, int>		third_node(3, 105);
 		ft::Node<int, int>		fourth_node(4, 103);
 		
-		bst.addNode(first_node);
+		bst.addNode(first_node, *bst.root);
+		std::cout << "value root: [" << bst.root->pair._value << "]" << std::endl; 
+		bst.addNode(second_node, *bst.root);
+		bst.addNode(third_node, *bst.root);
+		bst.addNode(fourth_node, *bst.root);
+		bst.printBst(*(bst.root));
+		tmp = bst.searchNode(105, *(bst.root));
+		if (tmp != 0x0)
+			std::cout << "print search: [" << tmp->pair._value << "]" << std::endl;
+		tmp = bst.searchNode(99, *(bst.root));
+		if (tmp != 0x0)
+			std::cout << "print search: [" << tmp->pair._value << "]" << std::endl;
+
+		bst.printBst(*(bst.root));
+		std::cout << "--------------" << std::endl;
+		bst.deleteNode(99, *(bst.root));
+		std::cout << "--------------" << std::endl;
+		bst.printBst(*(bst.root));
+		std::cout << "--------------" << std::endl;
+		bst.deleteNode(108, *(bst.root));
+		bst.printBst(*(bst.root));
+		std::cout << "--------------" << std::endl;
+		bst.deleteNode(103, *(bst.root));
+		bst.printBst(*(bst.root));
+		std::cout << "--------------" << std::endl;
 	}
 	std::cout << std::endl << YELLOW << "*****fin de test*****" << RESET << std::endl << std::endl;
 
--- templates/map/Bst.hpp
+++ templates/map/Bst.hpp
@@ -6,7 +6,7 @@
 /*   By: asimon <asimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 13:38:49 by asimon            #+#    #+#             */
-/*   Updated: 2022/12/26 14:26:00 by asimon           ###   ########.fr       */
+/*   Updated: 2022/12/26 18:56:49 by asimon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,9 @@ namespace ft{
 			};
 			
 			Node(){
+				ft::pair<Key, Value>		tmp;
 				this->parent = 0x0;
+				this->pair = tmp;
 				this->left = 0x0;
 				this->right = 0x0;
 				this->black = true;
@@ -123,36 +125,123 @@ namespace ft{
 		/*                              Methodes                                      */
 		////////////////////////////////////////////////////////////////////////////////
 		
-			void	addNode(node& newNode, node pos) {
+			void	addNode(node& newNode, node &pos) {
 				if (root == 0x0){
+					std::cout << "set root" << std::endl;
 					root = &newNode;
 					this->sentinel->left = root;
 					this->root->parent = this->sentinel;
+					this->root->left = sentinel;
+					this->root->right = sentinel;
 					return ;
 				}
 				else{
-					if (newNode->value < pos->value && pos->left == sentinel){
-						pos->left = newNode;
-						newNode->parent = pos;
-						newNode->isLeftChild = true;
-						newNode->left = sentinel;
-						newNode->right = sentinel;
+					if (newNode.pair._value == pos.pair._value)
+						return ;
+					if (newNode.pair._value < pos.pair._value && pos.left == sentinel){
+						pos.left = &newNode;
+						newNode.parent = &pos;
+						newNode.isLeftChild = true;
+						newNode.left = sentinel;
+						newNode.right = sentinel;
 					}
-					else if (newNode->value < pos->value && pos->left != sentinel)
-						addNode(newNode, pos->left);
-					else if (newNode->value > pos->value && pos->right == sentinel){
-						pos->right = newNode;
-						newNode->parent = pos;
-						newNode->left = sentinel;
-						newNode->right = sentinel;
-						newNode->isLeftChild = false;
+					else if (newNode.pair._value < pos.pair._value && pos.left != sentinel)
+						addNode(newNode, *pos.left);
+					else if (newNode.pair._value > pos.pair._value && pos.right == sentinel){
+						pos.right = &newNode;
+						newNode.parent = &pos;
+						newNode.left = sentinel;
+						newNode.right = sentinel;
+						newNode.isLeftChild = false;
 					}
 					else
-						addNode(newNode, pos->right);
+						addNode(newNode, *pos.right);
 					// checkcolor(newNode);
 				}
 				return ;
 			};
+
+			node*		searchNode(const value_type& value, node& pos){
+				if (root == 0x0){
+					std::cerr << "BST is empty" << std::endl;
+					return (0x0);
+				}
+				if (pos.sentinel == true){
+					std::cerr << "not in BST" << std::endl;
+					return (0x0);
+				}
+				if (value < pos.pair._value)
+					return (searchNode(value, *pos.left));
+				if (value > pos.pair._value)
+					return (searchNode(value, *pos.right));
+				if (value == pos.pair._value)
+					return (&pos);
+				return (0x0);
+			}
+
+			void		deleteNode(const value_type& value, node &pos){
+				node		*tmp;
+				std::cout << "start: " << "pos val: [" << pos.pair._value << "]" << "val : [" << value << "]" << std::endl; 
+				if (pos.pair._value == value){
+					std::cout << "equal" << std::endl;
+					tmp = pos.parent;
+					std::cout << "ici" << std::endl;
+					if (pos.isLeftChild == true && pos.left->sentinel == false){
+						tmp->left = pos.left;
+						tmp->left->right = pos.right;
+						pos.left->parent = tmp;
+						this->_alloc.destroy(&pos);
+					}
+					else if (pos.isLeftChild == true && pos.left->sentinel == true && pos.right->sentinel == false){
+						tmp->left = pos.right;
+						pos.right->parent = tmp;
+						this->_alloc.destroy(&pos);
+					}
+					else if (pos.isLeftChild == false && pos.left->sentinel == false){
+						tmp->right = pos.left;
+						tmp->right->right = pos.right;
+						pos.left->parent = tmp;
+						this->_alloc.destroy(&pos);
+					}
+					else if (pos.isLeftChild == false && pos.left->sentinel == true && pos.right->sentinel == false){
+						tmp->right = pos.right;
+						pos.right->parent = tmp;
+						this->_alloc.destroy(&pos);
+					}
+					else if (pos.isLeftChild == true){
+						tmp->left = pos.left;
+						pos.left->parent = tmp;
+						_alloc.destroy(&pos);
+					}
+					else if (pos.isLeftChild == false){
+						tmp->right = pos.right;
+						pos.right->parent = tmp;
+						_alloc.destroy(&pos);
+					}
+					return ;
+				}
+				else if (value < pos.pair._value && pos.left->sentinel == false){
+					std::cout << "less" << std::endl;
+					deleteNode(value, *(pos.left));
+					return ;
+				}
+				else if (value > pos.pair._value && pos.right->sentinel == false){
+					std::cout << "more" << std::endl;
+					deleteNode(value, *(pos.right));
+					return ;
+				}
+				std::cout << "end" << std::endl;
+				return ;
+			}
+
+			void		printBst(node& pos) const{
+				if (pos.left != sentinel)
+					printBst(*(pos.left));
+				std::cout << "value : [" << pos.pair._value  << "]" << std::endl;
+				if (pos.right != sentinel)
+					printBst(*(pos.right));
+				return ;
+			}
 
 	};
 
